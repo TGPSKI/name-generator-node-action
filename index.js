4715cd483a36366fb7170cc09cc67b72aa23076c
Tyler Pate <tyler@takescoop.com>
2020-05-17 18:34:15 -0700

    try ncc

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const core = require('@actions/core')
 const github = require('@actions/github')
 const { uniqueNamesGenerator, adjectives, colors, animals, names } = require('unique-names-generator')
